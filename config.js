d2fa6de4975ecbcf8e2cba9ab951316f19981bc0
BrightShadow <pporombka@gmail.com>
2016-04-18 14:38:35 +0200

    [config] Use snap.svg instead of d3 library

--- config.js
+++ config.js
@@ -1,12 +1,39 @@
 System.config({
-	defaultJSExtensions: true,
-	transpiler: "none",
-	paths: {
-		"*": "demo/*",
-		"npm:*": "jspm_packages/npm/*"
-	},
+  defaultJSExtensions: true,
+  transpiler: "none",
+  paths: {
+    "*": "demo/*",
+    "npm:*": "jspm_packages/npm/*",
+    "github:*": "jspm_packages/github/*"
+  },
 
-	map: {
-		"d3": "npm:d3@3.5.16"
-	}
+  map: {
+    "snapsvg": "npm:snapsvg@0.4.0",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:snapsvg@0.4.0": {
+      "eve": "npm:eve@0.4.2",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    }
+  }
 });
