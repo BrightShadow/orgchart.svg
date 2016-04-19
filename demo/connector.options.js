2210bfdab6b6b0ad581699dd798eeb25a4046eff
BrightShadow <pporombka@gmail.com>
2016-04-19 09:00:39 +0200

    Custom style for connector lines

---
+++ demo/connector.options.js
@@ -0,0 +1,11 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var ConnectorOptions = (function () {
+        function ConnectorOptions() {
+        }
+        return ConnectorOptions;
+    }());
+    exports.ConnectorOptions = ConnectorOptions;
+});
+//# sourceMappingURL=connector.options.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29ubmVjdG9yLm9wdGlvbnMuanMiLCJzb3VyY2VSb290IjoiL3NyYyIsInNvdXJjZXMiOlsiY29ubmVjdG9yLm9wdGlvbnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7SUFBQTtRQUFBO1FBR0EsQ0FBQztRQUFELHVCQUFDO0lBQUQsQ0FBQyxBQUhELElBR0M7SUFIWSx3QkFBZ0IsbUJBRzVCLENBQUEifQ==
