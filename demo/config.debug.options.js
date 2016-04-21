7464af838147094132de5ed1eaba1c664c338dd9
BrightShadow <pporombka@gmail.com>
2016-04-21 10:07:49 +0200

    Ignore demo folder and files

--- demo/config.debug.options.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-});
-//# sourceMappingURL=config.debug.options.js.map
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmRlYnVnLm9wdGlvbnMuanMiLCJzb3VyY2VSb290IjoiL3NyYyIsInNvdXJjZXMiOlsiY29uZmlnLmRlYnVnLm9wdGlvbnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
--- demo/org.chart.config.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-});
-//# sourceMappingURL=org.chart.config.js.map
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoib3JnLmNoYXJ0LmNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIvc3JjIiwic291cmNlcyI6WyJvcmcuY2hhcnQuY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
