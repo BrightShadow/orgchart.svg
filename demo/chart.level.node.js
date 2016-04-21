4b51873640b101ba333af7dc1d2122121ece718c
Paweł Porombka <prmp@viessmann.com>
2016-04-21 10:07:49 +0200

    Ignore demo folder and files

--- demo/chart.level.node.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-});
-//# sourceMappingURL=chart.level.node.js.map
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2hhcnQubGV2ZWwubm9kZS5qcyIsInNvdXJjZVJvb3QiOiIvc3JjIiwic291cmNlcyI6WyJjaGFydC5sZXZlbC5ub2RlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
--- demo/connector.options.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-});
-//# sourceMappingURL=connector.options.js.map
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29ubmVjdG9yLm9wdGlvbnMuanMiLCJzb3VyY2VSb290IjoiL3NyYyIsInNvdXJjZXMiOlsiY29ubmVjdG9yLm9wdGlvbnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
