2b064470f0bf9194d71f146123355b344ba78ec7
BrightShadow <pporombka@gmail.com>
2016-04-22 13:29:17 +0200

    Working unit testing env

--- build/paths.js
+++ build/paths.js
@@ -6,6 +6,8 @@ module.exports = {
 	output: outputRoot,
 	source: appRoot + '**/*.ts',
 	demoSrc: appRoot + 'demo.html',
+	testsSrc: './tests/**/*.ts',
+	testsDst: './tests',
 	dtsSrc: [
 		'./typings/browser/**/*.d.ts',
 		'./jspm_packages/**/*.d.ts'
---
+++ test/unit/example.spec.ts
@@ -0,0 +1,34 @@
+import {OrgChartSvg} from '../../src/orgchart.svg';
+
+
+describe('OrgChart.svg module', () => {
+  var sut, mockedRouter;
+
+  //beforeEach(() => {
+  //  mockedRouter = new RouterStub();
+  //  sut = new App();
+  //  sut.configureRouter(mockedRouter, mockedRouter);
+  //});
+  //
+  //it('contains a router property', () => {
+  //  expect(sut.router).toBeDefined();
+  //});
+  //
+  //it('configures the router title', () => {
+  //  expect(sut.router.title).toEqual('Aurelia');
+  //});
+  //
+  //it('should have a welcome route', () => {
+  //  expect(sut.router.routes).toContain({ route: ['','welcome'], name: 'welcome',  moduleId: 'welcome', nav: true, title:'Welcome' });
+  //});
+  //
+  //it('should have a users route', () => {
+  //   expect(sut.router.routes).toContain({ route: 'users', name: 'users', moduleId: 'users', nav: true, title:'Github Users' });
+  //});
+  //
+  it('should have a working unit test', () => {
+    expect(true).toBe(true);
+  });
+
+
+});
