b2cfea6fa8c9f08c9dab8b56602545b5b910495f
BrightShadow <pporombka@gmail.com>
2016-04-26 12:46:33 +0200

    Add index.html watch

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -4,13 +4,14 @@ var browserSync = require('browser-sync');
 
 // outputs changes to files to the console
 function reportChange(event) {
-  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
 
 // this task wil watch for changes
 // to js, html, and css files and call the
 // reportChange method. Also, by depending on the
 // serve task, it will instantiate a browserSync session
-gulp.task('watch', ['serve'], function() {
-  gulp.watch(paths.source, ['build-js', browserSync.reload]).on('change', reportChange);
+gulp.task('watch', ['serve'], function () {
+	gulp.watch(paths.source, ['build-js', browserSync.reload]).on('change', reportChange);
+	gulp.watch('index.html', ['build-js', browserSync.reload]).on('change', reportChange);
 });
