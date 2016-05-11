db687de5436c6243949c9ce3b64db2a08a6eed1f
BrightShadow <pporombka@gmail.com>
2016-05-11 23:00:11 +0200

    [#5][fix] Upgrade Snap.svg to 0.4.1 - 'E is not definied' bug was fixed there

--- config.js
+++ config.js
@@ -17,8 +17,9 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
+    "es6-promise": "npm:es6-promise@3.1.2",
     "snap.svg.zpd": "npm:snap.svg.zpd@0.0.11",
-    "snapsvg": "npm:snapsvg@0.4.0",
+    "snapsvg": "github:adobe-webplatform/Snap.svg@0.4.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -26,7 +27,7 @@ System.config({
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.2"
+      "process": "npm:process@0.11.3"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
@@ -43,20 +44,19 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
+    "npm:es6-promise@3.1.2": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:process@0.11.2": {
+    "npm:process@0.11.3": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
     "npm:snap.svg.zpd@0.0.11": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:snapsvg@0.4.0": {
-      "eve": "npm:eve@0.4.2",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:util@0.10.3": {
