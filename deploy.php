1063f4514802a54931406f5cc71f56b903da2d2d
BrightShadow <pporombka@gmail.com>
2016-06-07 19:57:34 +0000

    [config] Deployment script for Github Hooks

---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+/*
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+
+	// The commands
+	$commands = array(
+		'git status',
+		'git fetch origin deploy --depth=1',
+		'git reset --hard origin/deploy',
+		'rimraf node_modules',
+                'rimraf jspm_modules',
+		'npm install',
+		'jspm install',
+		'typings install',
+	);
+
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+
+	// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /         &copy; BrightShadow 2012 |
+              |____________________________|
+
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
